5c8e494ddd0dc4919cf32d22499a2d38ea44c477
Sue <sanschutz@suelanssmacbook.home>
2019-03-01 01:08:34 -0500

    Test the DELETE /todos/:id route

--- server/server.js
+++ server/server.js
@@ -58,7 +58,7 @@ app.delete('/todos/:id', (req, res) => {
         return res.status(404).send();
     };
 
-    Todo.findOneAndRemove(id).then((todo) => {
+    Todo.findByIdAndRemove(id).then((todo) => {
         if (!todo) {
             return res.status(404).send();
         }
